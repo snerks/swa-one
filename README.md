89b5515110317ef4c405883c42c4d5fb4616a1e1
snerks <martin.github@outlook.com>
2021-08-14 13:14:35 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 # SwaOne
 
+Install the CLI
+
+`npm install -g @azure/static-web-apps-cli`
+
+In the root folder, run:
+
+`swa start http://localhost:4200 --api=http://localhost:7071`
+
+# Notes
+
+Setting up local development environment
+
+`https://docs.microsoft.com/en-us/azure/static-web-apps/local-development`
+
+Notes on `text/plain` responses
+
+`https://angular.io/guide/http`
+
+```Typescript
+    const headers = new HttpHeaders().set(
+      'Accept',
+      'text/plain; charset=utf-8'
+    );
+
+    const response$: Observable<string> = this.http.get(
+      '/api/Greetings?name=Bob',
+      {
+        headers,
+        responseType: 'text',
+      }
+    );
+```
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.1.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'swa-one';
+  greeting?: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const headers = new HttpHeaders().set(
+      'Accept',
+      'text/plain; charset=utf-8'
+    );
+
+    const response$: Observable<string> = this.http.get(
+      '/api/Greetings?name=Bob',
+      {
+        headers,
+        responseType: 'text',
+      }
+    );
+
+    response$.subscribe(
+      (data) => (this.greeting = data),
+      (error) => console.error(error)
+    );
+  }
 }
